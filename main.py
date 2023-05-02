588f2399e95ee457137b0f67b4e3058252d78515
kobi <kobikahn@gmail.com>
2023-05-02 12:32:54 -0400

    IM FINISHED

--- main.py
+++ main.py
@@ -18,6 +18,63 @@ def plot_hist(h_list):
         plt.title(f'{col}')
         plt.show()
 
+def get_percent(num1, num2):
+    percent1 = (num1 / (num1 + num2)) * 100
+    percent2 = (num2 / (num1 + num2)) * 100
+    return round(percent1, 2), round(percent2, 2)
+
+def calc_demographics(demo):
+    if demo == 'all':
+        NCHD = len(heart0_df)
+        CHD = len(heart1_df)
+        no_percent, yes_percent = get_percent(NCHD, CHD)
+        print(f'NCHD: {NCHD}     CHD: {CHD}\nNCHD: {no_percent}%     CHD: {yes_percent}%')
+
+    elif demo == 'age':
+        LE45 = heart_df.index[heart_df['age'] <= 45].tolist()
+        GT45 = heart_df.index[heart_df['age'] > 45].tolist()
+        LE45_df = heart_df.drop(GT45)
+        GT45_df = heart_df.drop(LE45)
+        LE45_no = len(LE45_df.index[LE45_df['TenYearCHD'] == 0].tolist())
+        LE45_yes = len(LE45_df.index[LE45_df['TenYearCHD'] == 1].tolist())
+
+        GT45_no = len(GT45_df.index[GT45_df['TenYearCHD'] == 0].tolist())
+        GT45_yes = len(GT45_df.index[GT45_df['TenYearCHD'] == 1].tolist())
+        LE_no_percent, LE_yes_percent = get_percent(LE45_no, LE45_yes)
+        GT_no_percent, GT_yes_percent = get_percent(GT45_no, GT45_yes)
+        print(f'LE45   NCHD: {LE45_no}     CHD: {LE45_yes}\nGT45   NCHD: {GT45_no}     CHD: {GT45_yes}\nLE45: NCHD: {LE_no_percent}%     CHD: {LE_yes_percent}%\nGT45   NCHD:{GT_no_percent}%     CHD: {GT_yes_percent}%')
+
+    elif demo == 'gender':
+        M_num = heart_df.index[heart_df['male'] == 1].tolist()
+        F_num = heart_df.index[heart_df['male'] == 0].tolist()
+        M_df = heart_df.drop(F_num)
+        F_df = heart_df.drop(M_num)
+        M_no = len(M_df.index[M_df['TenYearCHD'] == 0].tolist())
+        M_yes = len(M_df.index[M_df['TenYearCHD'] == 1].tolist())
+        F_no = len(F_df.index[F_df['TenYearCHD'] == 0].tolist())
+        F_yes = len(F_df.index[F_df['TenYearCHD'] == 1].tolist())
+        M_no_percent, M_yes_percent = get_percent(M_no, M_yes)
+        F_no_percent, F_yes_percent = get_percent(F_no, F_yes)
+        print(f'NCHD   Male: {M_no}     Female: {F_no}\nCHD   Male: {M_yes}     Female: {F_yes}\nMale   NCHD: {M_no_percent}%     CHD: {M_yes_percent}%\nFemale   NCHD: {F_no_percent}%     CHD: {F_yes_percent}%')
+
+    elif demo == 'education':
+        ed_1_df = heart_df[heart_df['education'] == 1]
+        ed_2_df = heart_df[heart_df['education'] == 2]
+        ed_3_df = heart_df[heart_df['education'] == 3]
+        ed_4_df = heart_df[heart_df['education'] == 4]
+        ed1_no = len(ed_1_df.index[ed_1_df['TenYearCHD'] == 0].tolist())
+        ed1_yes = len(ed_1_df.index[ed_1_df['TenYearCHD'] == 1].tolist())
+        ed2_no = len(ed_2_df.index[ed_2_df['TenYearCHD'] == 0].tolist())
+        ed2_yes = len(ed_2_df.index[ed_2_df['TenYearCHD'] == 1].tolist())
+        ed3_no = len(ed_3_df.index[ed_3_df['TenYearCHD'] == 0].tolist())
+        ed3_yes = len(ed_3_df.index[ed_3_df['TenYearCHD'] == 1].tolist())
+        ed4_no = len(ed_4_df.index[ed_4_df['TenYearCHD'] == 0].tolist())
+        ed4_yes = len(ed_4_df.index[ed_4_df['TenYearCHD'] == 1].tolist())
+        ed1_no_percent, ed1_yes_percent = get_percent(ed1_no, ed1_yes)
+        ed2_no_percent, ed2_yes_percent = get_percent(ed2_no, ed2_yes)
+        ed3_no_percent, ed3_yes_percent = get_percent(ed3_no, ed3_yes)
+        ed4_no_percent, ed4_yes_percent = get_percent(ed4_no, ed4_yes)
+        print(f'Level 1   NCHD: {ed1_no}     CHD: {ed1_yes}\nLevel 2   NCHD: {ed2_no}     CHD: {ed2_yes}\nLevel 3   NCHD: {ed3_no}     CHD: {ed3_yes}\nLevel 4   NCHD: {ed4_no}     CHD: {ed4_yes}\n\nLevel 1   NCHD: {ed1_no_percent}%     CHD: {ed1_yes_percent}%\nLevel 2   NCHD: {ed2_no_percent}%     CHD: {ed2_yes_percent}%\nLevel 3   NCHD: {ed3_no_percent}%     CHD: {ed3_yes_percent}%\nLevel 4   NCHD: {ed4_no_percent}%     CHD: {ed4_yes_percent}%')
 
 # CREATE THE TWO SEPERATE HEART DF
 heart_df = pd.read_csv('Heart_stuff.csv', delim_whitespace=False)
@@ -33,12 +90,12 @@ heart0_df = heart0_df.reset_index()
 # print(heart1_df)
 # print()
 # print(heart0_df)
-hist_list = ['totChol', 'BMI', 'sysBP', 'diaBP', 'glucose', 'heartRate', 'cigsPerDay']
-plot_hist(hist_list)
+plot = False
+if plot:
+    hist_list = ['totChol', 'BMI', 'sysBP', 'diaBP', 'glucose', 'heartRate', 'cigsPerDay']
+    plot_hist(hist_list)
 
-# for each risk
-# factor for those who are not at risk and those that are.
-# print(heart_df)
+calc_demographics('education')
 
 
 
